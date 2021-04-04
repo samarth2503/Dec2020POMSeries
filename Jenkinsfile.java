44ee39ab0ac80f106b23b8b6f3081f979f93bf5c
samarth2503 <samarthjain680@gmail.com>
2021-04-04 19:02:35 +0530

    Jenkins File

---
+++ Jenkinsfile.java
@@ -0,0 +1,53 @@
+pipeline { 
+agent any 
+    stages { 
+        
+        stage ('Build') { 
+            steps{
+                echo "Building the test automation for demo cart app"
+            }
+        }
+        
+        stage('Test') {
+            steps {
+		
+                catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+                	bat git credentialsId: 'gitCred', url: 'https://github.com/samarth2503/Dec2020POMSeries'
+                    bat "mvn clean install"
+                }
+            }
+        }
+                
+     
+        stage('Publish Allure Reports') {
+           steps {
+                script {
+                    allure([
+                        includeProperties: false,
+                        jdk: '',
+                        properties: [],
+                        reportBuildPolicy: 'ALWAYS',
+                        results: [[path: '/allure-results']]
+                    ])
+                }
+            }
+        }
+        
+        
+        stage('Publish Extent Report'){
+            steps{
+                     publishHTML([allowMissing: false,
+                                  alwaysLinkToLastBuild: false, 
+                                  keepAll: false, 
+                                  reportDir: 'build', 
+                                  reportFiles: 'TestExecutionReport.html', 
+                                  reportName: 'HTML Extent Report', 
+                                  reportTitles: ''])
+            }
+        }
+        
+        
+        
+    }
+
+ }
